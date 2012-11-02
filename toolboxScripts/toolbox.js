7b35091e84097c23a13d16344eab5fdbfcf1a3bb
Mateusz <mkowalski@graal.pl>
2012-11-03 00:39:18 +0100

    porzadki

--- toolboxScripts/toolbox.js
+++ toolboxScripts/toolbox.js
@@ -1,5 +1,10 @@
 ﻿$(function () {
 
+
+
+ 
+
+
     $("#dialog-picture-form").dialog({
         autoOpen: false,
         height: 450,
@@ -36,7 +41,7 @@
         receive: function (event, ui) {
             var newItem = $(this).data().sortable.currentItem;
             newItem.html("");
-            newItem.attr('id', ui.item.attr('id') + 's');
+            
             switch (ui.item.attr('id')) {
                 case 'textEditor':
                     newItem.addClass("textEditor").text("Kliknij dwa razy, aby edytować sekcję!");
@@ -44,9 +49,7 @@
                     break;
                 case 'accordion':
                     newItem.html("<div class='accordion'><h3 >Section 1</h3><div><p></p></div><h3>Section 2</h3><div><p></p></div></div>");
-                    $(".accordion").accordion();
-                 
-                    
+                    $(".accordion").accordion();    
                     break;
                 case 'picture':
                     $("#dialog-picture-form").data('element', '#' + ui.item.attr('id') + 's');
@@ -64,40 +67,9 @@
         appendTo: "#contentUL",
         revert: "invalid",
         helper: "clone"
-        //            start: function (event, ui) {
-        //                $("<li></li>").addClass("ui-state-highlight").attr({ id: "helper", style: "width: 100% ; height: 20px" }).appendTo("#contentUL");
-        //            },
-        //            stop: function (event, ui) {
-        //                $("#helper").remove();
-
-
-        //            }
+    
     });
 
-
-
-
-    function addTool($item) {
-        alert("addtool");
-        console.log($item);
-        var id = $item.attr('id');
-        var rnd = Math.floor((Math.random() * 10) + 1);
-        $($item).attr('id', id);
-        //        $("<li></li>").attr('id', id + rnd).appendTo("#contentUL");
-        //        switch (id + rnd) {
-        //            case 'textEditor' + rnd:
-        //                alert("editor");
-        //                var config = {};
-        //                CKEDITOR.appendTo(id + rnd, config, "sadasdasdasdasdsdfgdfg dfgh fhfg hjgj fg");
-        //                break;
-
-        //            case 'test' + rnd:
-        //                alert("test");
-        //                $('#' + id + rnd).html("cccccc");
-        //                break;
-        //        }
-
-    }
 
     $("#footer").click(function () {
         console.log($('html')[0].outerHTML);
